ff9b156b8b127412c74c52f7b9785cb7aa8fc166
Nguyen Nam <nghoofnam@gmail.com>
2025-09-26 16:53:23 +0700

    Remove __pycache__ and update .gitignore

--- docker/db/initdb.d/init_data.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "Loading data into online_retail table..."
-
-psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c "\copy online_retail FROM '/data/online_retail.csv' WITH CSV HEADER DELIMITER ','"
