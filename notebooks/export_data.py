063518a2f6d37027c00d89e929998cdf05762b9a
Nguyen Nam <nghoofnam@gmail.com>
2025-10-04 23:21:42 +0700

    fix requirement.txt and update export_data with SQLAlchemy version

--- notebooks/export_data.py
+++ notebooks/export_data.py
@@ -2,8 +2,13 @@ from sqlalchemy import create_engine
 import pandas as pd
 from pathlib import Path
 
-DB_URI = "postgresql+psycopg2://your_username:your_password@localhost:5432/fp_growth_mba"
-engine = create_engine(DB_URI)
+DB_CONFIG = {
+    "host": "localhost",
+    "port": "5432",
+    "dbname": "db_postgres",
+    "user": "nhnammldlnlpcvrs",
+    "password": "abc123"
+}
 
 OUTPUT_DIR = Path("../data")
 OUTPUT_DIR.mkdir(exist_ok=True)
@@ -11,15 +16,20 @@ OUTPUT_DIR.mkdir(exist_ok=True)
 MODELS = ["transaction_fpgrowth", "user_item_dl"]
 
 def export_models():
-    with engine.connect() as conn:
-        for model in MODELS:
-            print(f"Exporting {model}...")
+    conn_str = (
+        f"postgresql+psycopg2://{DB_CONFIG['user']}:{DB_CONFIG['password']}"
+        f"@{DB_CONFIG['host']}:{DB_CONFIG['port']}/{DB_CONFIG['dbname']}"
+    )
+    engine = create_engine(conn_str)
 
-            df = pd.read_sql(f"SELECT * FROM {model};", conn)
+    for model in MODELS:
+        print(f"Exporting {model}...")
 
-            output_file = OUTPUT_DIR / f"{model}.csv"
-            df.to_csv(output_file, index=False)
-            print(f"Saved {len(df)} rows to {output_file}")
+        df = pd.read_sql(f"SELECT * FROM {model};", engine)
+
+        output_file = OUTPUT_DIR / f"{model}.csv"
+        df.to_csv(output_file, index=False)
+        print(f"Saved to {output_file}")
 
 if __name__ == "__main__":
     export_models()
